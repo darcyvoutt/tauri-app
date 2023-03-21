e1769ba342bd40fafa3cae333f3cb3d3f4a55f93
Darcy Voutt <darcyvoutt@gmail.com>
2023-03-21 18:35:01 -0400

    Adding support for draggable header

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,13 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import Audio from "./components/Audio.vue";
-import Greet from "./components/Greet.vue";
-import Welcome from "./components/Welcome.vue";
 </script>
 
 <template>
-  <div class="h-screen flex flex-col items-center justify-center">
-    <Audio />
+  <div>
+    <header data-tauri-drag-region class="faux-header"></header>
+    <div class="h-screen flex flex-col items-center justify-center">
+      <Audio />
+    </div>
   </div>
 </template>
