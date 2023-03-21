1788678f1148d7d127e484e355a908cda009f526
Darcy Voutt <darcyvoutt@gmail.com>
2023-03-21 17:23:33 -0400

    Remove internal api call for now

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,9 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import { onMounted } from 'vue'
-
 import Audio from "./components/Audio.vue";
 import Greet from "./components/Greet.vue";
 import Welcome from "./components/Welcome.vue";
-
-import { invoke } from "@tauri-apps/api/tauri";
-
-async function getData() {
-  return await invoke("fetch", { name: name.value });
-}
-
-onMounted(() => {
-  this.getData()
-})
 </script>
 
 <template>
