a0993f1411a38b38111393ed8cfc48849acb9ec3
Darcy Voutt <darcyvoutt@gmail.com>
2023-03-21 19:55:16 -0400

    Setting up storage test

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import Audio from "./components/Audio.vue";
+import Storage from "./components/Storage.vue";
 </script>
 
 <template>
@@ -9,6 +10,7 @@ import Audio from "./components/Audio.vue";
     <header data-tauri-drag-region class="faux-header"></header>
     <div class="h-screen flex flex-col items-center justify-center">
       <Audio />
+      <Storage />
     </div>
   </div>
 </template>
---
+++ src/components/Storage.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  data() {
+    return {
+      input: null,
+      storage: null,
+    }
+  },
+  watch: {
+    input(input) {
+      localStorage.setItem("test", input)
+    }
+  },
+  mounted() {
+    this.storage = localStorage.getItem("test")
+  },
+}
+</script>
+
+<template>
+  <div class="pt-2">
+    <form>
+      <input type="text" v-model="input" class="py-2.5 px-4 border-2 border-neutral-300 rounded-md" />
+    </form>
+
+    <p>Storage: {{ storage }}</p>
+  </div>
+</template>
