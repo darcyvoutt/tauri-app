4b3abba2f6444a70aca08f2309316ae184461027
Darcy Voutt <darcyvoutt@gmail.com>
2023-03-20 22:04:31 -0400

    setting up audio test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Audio from "./components/Audio.vue";
 import Greet from "./components/Greet.vue";
 </script>
 
@@ -38,6 +39,7 @@ import Greet from "./components/Greet.vue";
     </p>
 
     <Greet />
+    <Audio />
   </div>
 </template>
 
---
+++ src/components/Audio.vue
@@ -0,0 +1,10 @@
+<script setup>
+import file from "../assets/piano.mp3"
+const audioFile = file
+</script>
+
+<template>
+  <audio ref="audio" :src="audioFile" controls></audio>
+
+  <button @click="$refs.audio.play()">Play Audio</button>
+</template>
