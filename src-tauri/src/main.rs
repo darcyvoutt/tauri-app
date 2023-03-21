7500366994eb9fc649efdeb80b7af772ddbdb3c6
Darcy Voutt <darcyvoutt@gmail.com>
2023-03-20 22:32:28 -0400

    Setting up to do api fetch

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,12 +4,23 @@
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+  format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![greet])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
+
+use reqwest::Result;
+
+async fn fetch() -> Result<()> {
+    let response = reqwest::get("https://jsonplaceholder.typicode.com/todos/1")
+        .await?
+        .text()
+        .await?;
+    println!("Response: {:?}", response);
+    Ok(())
 }
