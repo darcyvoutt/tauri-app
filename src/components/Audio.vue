095df8d5771c5f6fd2b0923a4d20fd6c3b57fd60
Darcy Voutt <darcyvoutt@gmail.com>
2023-03-23 19:27:26 -0400

    volume control for audio player

--- src/components/Audio.vue
+++ src/components/Audio.vue
@@ -4,23 +4,31 @@ export default {
   data() {
     return {
       file: file,
+      volume: 0.2,
     };
+  },
+  computed: {
+    currentVolume() {
+      return this.volume * 100
+    },
   },
   methods: {
     play() {
-      this.$refs.audio.volume = 0.5;
-      this.$refs.audio.play();
+      this.$refs.audio.volume = this.volume
+      this.$refs.audio.play()
     },
   }
 };
 </script>
 
 <template>
-  <div>
-    <audio ref="audio" :src="file"></audio>
-
+  <div class="flex items-center space-x-6">
     <button class="btn" @click="play()">
       Play Audio
     </button>
+    <input type="range" min="0" max="1" step="0.1" v-model="volume">
+    <span>{{ currentVolume }}</span>
+
+    <audio ref="audio" :src="file"></audio>
   </div>
 </template>
