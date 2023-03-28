57cfb887708ab8a91a933fa5282b62fad2bcc470
Darcy Voutt <darcyvoutt@gmail.com>
2023-03-28 18:33:33 -0400

    remove greet command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,12 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
 use machine_uid;
 
 #[tauri::command]
@@ -17,7 +11,7 @@ fn machine_id() -> String {
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet, machine_id])
+        .invoke_handler(tauri::generate_handler![machine_id])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
