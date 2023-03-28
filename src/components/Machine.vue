f2f8ac19aee95c19753837a38709f2cb2934134b
Darcy Voutt <darcyvoutt@gmail.com>
2023-03-28 18:32:52 -0400

    Adding in machine component

---
+++ src/components/Machine.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import { invoke } from "@tauri-apps/api/tauri";
+
+const uuid = ref("");
+
+async function machine() {
+  uuid.value = await invoke("machine_id");
+}
+
+onMounted(() => {
+  machine()
+})
+</script>
+
+<template>
+  <div class="flex items-center space-x-4">
+    <span class="font-bold">Machine ID: </span>
+    <pre class="pre flex-grow">{{ uuid }}</pre>
+  </div>
+</template>
