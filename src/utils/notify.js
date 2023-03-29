4dd5cabec206026426513e2c902402e3a222c0c1
Darcy Voutt <darcyvoutt@gmail.com>
2023-03-28 20:59:14 -0400

    move awaits inside of async method

--- src/utils/notify.js
+++ src/utils/notify.js
@@ -1,15 +1,15 @@
 import { isPermissionGranted, requestPermission, sendNotification } from '@tauri-apps/api/notification';
 import { getName } from '@tauri-apps/api/app';
 
-const appName = await getName();
-let permissionGranted = await isPermissionGranted();
-
-if (!permissionGranted) {
-  const permission = await requestPermission();
-  permissionGranted = permission === 'granted';
-}
-
 export async function notify(body) {
+  const appName = await getName();
+  let permissionGranted = await isPermissionGranted();
+
+  if (!permissionGranted) {
+    const permission = await requestPermission();
+    permissionGranted = permission === 'granted';
+  }
+
   console.log('permissionGranted', permissionGranted)
 
   if (permissionGranted) {
