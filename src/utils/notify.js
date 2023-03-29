54467650b3c2264bc3507f34c0632858c1acd652
Darcy Voutt <darcyvoutt@gmail.com>
2023-03-28 22:12:50 -0400

    Update notify util

--- src/utils/notify.js
+++ src/utils/notify.js
@@ -1,6 +1,16 @@
 import { isPermissionGranted, requestPermission, sendNotification } from '@tauri-apps/api/notification';
 import { getName } from '@tauri-apps/api/app';
 
+/**
+ *
+ * Note: There is a known issue with this API in which only works with Alert type.
+ * The Banners which is default does not work when app is in focus.
+ * Reference: https://github.com/tauri-apps/tauri/issues/5362
+ *
+ * @param {string} body - Message to display in notification
+ * @returns
+ */
+
 export async function notify(body) {
   if (!await isPermissionGranted()) {
     const permissionResult = await requestPermission();
