ca688f56f0daf5c5cdf7c2a3934d5112874f2def
Darcy Voutt <darcyvoutt@gmail.com>
2023-03-31 21:34:07 -0400

    Setting up function for hashing string

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+// Machine ID
 use machine_uid;
 
 #[tauri::command]
@@ -9,9 +10,21 @@ fn machine_id() -> String {
     format!("{}", id.to_lowercase())
 }
 
+// Hash Key validation
+use sha2::{Digest, Sha256};
+
+#[tauri::command]
+fn hash_string() -> String {
+    let mut hasher = Sha256::new();
+    let str = b"c15682a8-0eab-54ad-a902-4f43a69c72b8|6831453175974|2022-03-21T18:26:55.395Z";
+    hasher.update(str);
+    let hash = hasher.finalize();
+    format!("{:x}", hash)
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![machine_id])
+        .invoke_handler(tauri::generate_handler![machine_id, hash_string])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/components/Machine.vue
+++ src/components/Machine.vue
@@ -3,19 +3,32 @@ import { ref, onMounted } from "vue";
 import { invoke } from "@tauri-apps/api/tauri";
 
 const uuid = ref("");
+const license = ref("");
 
 async function machine() {
   uuid.value = await invoke("machine_id");
 }
 
+async function license_key() {
+  license.value = await invoke("hash_string");
+}
+
 onMounted(() => {
   machine()
+  license_key()
 })
 </script>
 
 <template>
-  <div class="flex items-center space-x-4">
-    <span class="font-bold">Machine ID: </span>
-    <pre class="pre flex-grow">{{ uuid }}</pre>
+  <div class="space-y-4">
+    <div class="grid grid-cols-6 items-center space-x-4">
+      <span class="col-span-1 font-bold">Machine ID: </span>
+      <pre class="col-span-5 pre">{{ uuid }}</pre>
+    </div>
+
+    <div class="grid grid-cols-6 items-center space-x-4">
+      <span class="col-span-1 font-bold">License: </span>
+      <pre class="col-span-5 pre">{{ license }}</pre>
+    </div>
   </div>
 </template>
