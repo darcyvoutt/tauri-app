f49c33df3c630a8bdf03715fcddb2f3f8f6a1535
Darcy Voutt <darcyvoutt@gmail.com>
2023-04-01 23:15:59 -0400

    setup data for api writing to data file, wright now just logging directory

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Api from "./components/Api.vue";
 import Audio from "./components/Audio.vue";
+import Data from "./components/Data.vue";
 import Local from "./components/Local.vue";
 import Machine from "./components/Machine.vue";
 import Notification from "./components/Notification.vue";
@@ -12,6 +13,7 @@ import Notification from "./components/Notification.vue";
   <main class="h-screen flex flex-col items-center justify-center">
     <div class="max-w-4xl w-10/12 border borders divide-y border-dividers rounded-md shadow-md">
       <Audio class="p-4" />
+      <Data class="p-4" />
       <Local class="p-4" />
       <Api class="p-4" />
       <Machine class="p-4" />
---
+++ src/components/Data.vue
@@ -0,0 +1,25 @@
+<script>
+import { appDataDir } from '@tauri-apps/api/path'
+
+export default {
+  data() {
+    return {
+      input: null,
+    };
+  },
+  mounted() {
+    this.appDirs()
+  },
+  methods: {
+    async appDirs() {
+      console.log('APP DATA:', await appDataDir())
+    }
+  },
+}
+</script>
+
+<template>
+  <div>
+
+  </div>
+</template>
