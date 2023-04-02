b5d8ffdb9d22813850355f61723272f12a57e05b
Darcy Voutt <darcyvoutt@gmail.com>
2023-04-02 12:35:14 -0400

    rewrite package scripts to be more clear

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -11,30 +11,32 @@ fn machine_id() -> String {
 }
 
 // Hash Key validation
+use dotenv::dotenv;
 use sha2::{Digest, Sha256};
 
 #[tauri::command]
 fn hash_string() -> String {
+    dotenv().ok();
+
+    // Get Secret from ENV Variables
+    let secret: String = std::env::var("SECRET").expect("$SECRET is not set");
+    let secret_str: &str = &secret[..];
+
+    println!("{}", secret_str);
+
+    // Get other variables
     let machine: String = String::from(machine_id());
     let customer: &str = "6831453175974";
     let timestamp: &str = "2022-03-21T18:26:55.395Z";
-    // let secret: String = std::env::var("SECRET").expect("$SECRET is not set");
-    // let secret_str: &str = &secret[..];
-    // + "|" + secret_str
-    // println!("{}", secret_str);
 
-    let key: String = machine + "|" + customer + "|" + timestamp;
+    let key: String = machine + "|" + customer + "|" + timestamp + "|" + secret_str;
     let mut hash = Sha256::new();
     hash.update(key);
     let hashed = hash.finalize();
     format!("{:x}", hashed)
 }
 
-use dotenv::dotenv;
-
 fn main() {
-    dotenv().ok();
-
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![machine_id, hash_string])
         .run(tauri::generate_context!())
